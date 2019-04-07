0f14d9310e673a9d5e30987e7a4c10cf5954eb88
Alex Smetukh <alex.smetukh@gmail.com>
2019-04-07 23:57:06 +0300

    searchbar done

---
+++ searchbar.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+    var SearchBar = {};
+    SearchBar.prototype = {
+      init: function() {
+        var searchbar = document.getElementById('search-bar');
+        var button = document.getElementById('search-button');
+        button.addEventListener('click', this.onclick.bind(this));
+      },
+      onclick: function() {
+        this.dispatchValue();
+      },
+      dispatchValue: function() {
+        var searchvalue = document.getElementById('search-value');
+  
+        if(searchvalue.value === '') return; 
+        var moviePayloadEvent = new CustomEvent('searchMovies', { 'detail': searchvalue.value });
+        document.dispatchEvent(moviePayloadEvent);
+      }
+    }
+    SearchBar.prototype.init();
+  }());
