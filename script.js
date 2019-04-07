2110ed4f0737602dd8b296ebe01b41e6ece0005e
Alex Smetukh <alex.smetukh@gmail.com>
2019-04-07 23:55:09 +0300

    add onclick

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 var searchbar = document.getElementById('search-bar');
 var button = document.getElementById('search-button');
 button.addEventListener('click', this.onclick.bind(this));
+onclick: function () {
+    this.dispatchValue();
+}
+dispatchValue: function() {
+var searchvalue = document.getElementById('search-value');
+
+if(searchvalue.value === '') return; 
+var moviePayloadEvent = new CustomEvent('searchMovies', { 'detail': searchvalue.value });
+document.dispatchEvent(moviePayloadEvent);
+}
