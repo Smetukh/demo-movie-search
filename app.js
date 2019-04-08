5a878e7ff86b6e862dbe9ee59ebb3fe205136bb6
Alex Smetukh <alex.smetukh@gmail.com>
2019-04-08 18:59:04 +0300

    render search results

--- app.js
+++ app.js
@@ -5,16 +5,29 @@
       init: function() {
         document.addEventListener('searchMovies', function (e) {
           var queryString = encodeURIComponent(e.detail);
-          theMovieDb.search.getMovie({"query":queryString}, onMoviesRetrieved, onMoviesError);
+          console.log('e.detail = ', e.detail)
+          fetch(`https://api.themoviedb.org/3/search/movie?api_key=81cfc9e7f03740911f7568ce112347b3&language=en-US&query=${queryString}&page=1&include_adult=false`)
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            console.log(JSON.stringify(data));
+            console.log('##########################');
+            console.log(data);
+            SearchResults.populate();
+            // var json = JSON.parse(data);
+            console.log('json = ')
+            SearchResults.populate(data.results);
+          })
         }, false);
       }
     }
   
-    var onMoviesRetrieved = function(data) {
-      SearchResults.populate();
-      var json = JSON.parse(data);
-      SearchResults.populate(json.results);
-    };
+    // var onMoviesRetrieved = function(data) {
+    //   SearchResults.populate();
+    //   var json = JSON.parse(data);
+    //   SearchResults.populate(json.results);
+    // };
   
     var onMoviesError = function(error) {
       console.log(error);
